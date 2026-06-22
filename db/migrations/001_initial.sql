36fe65f3f349dadb2224b4c34513c7df8ca04322
Brian Yoon <byoonie@gmail.com>
2026-06-22 05:46:27 -0700

    Add initial transaction schema

--- db/migrations/001_initial.sql
+++ db/migrations/001_initial.sql
@@ -1,6 +1,29 @@
 CREATE TABLE IF NOT EXISTS accounts (
-    id          INTEGER PRIMARY KEY,
-    name        TEXT NOT NULL,
-    on_budget   BOOLEAN NOT NULL,
-    closed      BOOLEAN NOT NULL
+    id                  INT PRIMARY KEY,
+    name                TEXT NOT NULL,
+    on_budget           BOOLEAN NOT NULL DEFAULT TRUE,
+    closed              BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS payees (
+    id                  INT PRIMARY KEY,
+    name                TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS budget_categories (
+    id                  INT PRIMARY KEY,
+    name                TEXT NOT NULL UNIQUE,
+    hidden              BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id                  INT PRIMARY KEY,
+    account_id          INT NOT NULL REFERENCES accounts(id),
+    payee_id            INT REFERENCES payees(id),
+    budget_category_id  INT REFERENCES budget_categories(id),
+    transaction_date    TEXT NOT NULL,
+    created_at          TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    notes               TEXT,
+    amount              INT NOT NULL,
+    cleared             BOOLEAN NOT NULL DEFAULT FALSE
 );
