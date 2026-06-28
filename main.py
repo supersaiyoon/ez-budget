7dc60ca6baf87261893862106e7063564c3246d2
Brian Yoon <byoonie@gmail.com>
2026-06-28 05:49:28 -0700

    Add comments to main()

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-﻿import sys
-
-from PyQt6.QtWidgets import QApplication
+﻿from PyQt6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
 
+# Only needed to access command line arguments
+import sys
+
 
 def main():
+    # Only one QApplication instance needed per application
+    # Pass in sys.argv to allow command line arguments for app
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    # Windows hidden by default
     window.show()
+    
     return app.exec()
 
 
