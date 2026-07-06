08e1e882f6480ab88fbc209a2d3c01037d1e5308
Brian Yoon <byoonie@gmail.com>
2026-07-06 05:13:09 -0700

    Update `id` column data type to INTEGER for all tables

--- db/migrations/001_initial.sql
+++ db/migrations/001_initial.sql
@@ -1,6 +1,6 @@
 -- Accounts kept separate so transactions can support multiple balances later
 CREATE TABLE IF NOT EXISTS accounts (
-    id                  INT PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY,
     name                TEXT NOT NULL,
     on_budget           BOOLEAN NOT NULL DEFAULT TRUE,
     closed              BOOLEAN NOT NULL DEFAULT FALSE
@@ -8,20 +8,20 @@ CREATE TABLE IF NOT EXISTS accounts (
 
 -- Normalize payees to avoid repeated merchants
 CREATE TABLE IF NOT EXISTS payees (
-    id                  INT PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY,
     name                TEXT NOT NULL UNIQUE
 );
 
 -- Normalize master budget categories
 CREATE TABLE IF NOT EXISTS master_budget_categories (
-    id                  INT PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY,
     name                TEXT NOT NULL UNIQUE,
     hidden              BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Budgets and transactions can share labels
 CREATE TABLE IF NOT EXISTS budget_categories (
-    id                          INT PRIMARY KEY,
+    id                          INTEGER PRIMARY KEY,
     master_budget_category_id   INT NOT NULL REFERENCES master_budget_categories(id),
     name                        TEXT NOT NULL UNIQUE,
     hidden                      BOOLEAN NOT NULL DEFAULT FALSE
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS budget_categories (
 
 -- Amount stored as integer to avoid floating point drift
 CREATE TABLE IF NOT EXISTS transactions (
-    id                  INT PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY,
     account_id          INT NOT NULL REFERENCES accounts(id),
     payee_id            INT REFERENCES payees(id),
     budget_category_id  INT REFERENCES budget_categories(id),
